a1700f5e6cb566209b41ad6bb18479c0fdeb8aec
Eliel Haouzi <eliel.haouzi@gmail.com>
2020-09-02 13:13:06 +0300

    fix fetching the repo for queutopia for an env not configured

--- lib/captain_hook/queue/queue.ex
+++ lib/captain_hook/queue/queue.ex
@@ -1,5 +1,5 @@
 defmodule CaptainHook.Queue do
   use Queuetopia,
-    repo: Application.fetch_env!(:captain_hook, :repo),
+    repo: Application.get_env(:captain_hook, :repo),
     performer: CaptainHook.Performer
 end
