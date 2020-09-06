0689587e9c635d03b7d748e54b805c66931b4c02
Eliel Haouzi <eliel.haouzi@gmail.com>
2020-09-06 15:59:54 +0300

    fix migration file of v2 for running query

--- lib/captain_hook/migrations/v2.ex
+++ lib/captain_hook/migrations/v2.ex
@@ -15,12 +15,12 @@ defmodule CaptainHook.Migrations.V2 do
     query =
       "ALTER TABLE captain_hook_webhook_endpoints ADD COLUMN headers JSON NULL AFTER metadata;"
 
-    Ecto.Adapters.SQL.query!(Application.fetch_env!(:captain_hook, :repo), query, [])
+    repo().query!(query)
   end
 
   defp alter_webhook_endpoints_table_drop_headers_column do
     query = "ALTER TABLE captain_hook_webhook_endpoints DROP COLUMN headers;"
 
-    Ecto.Adapters.SQL.query!(Application.fetch_env!(:captain_hook, :repo), query, [])
+    repo().query!(query)
   end
 end
