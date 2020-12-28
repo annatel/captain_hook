4d1c6706caeb280cef8146d8867b63c89a4feb82
Eliel Haouzi <eliel.haouzi@gmail.com>
2020-12-28 15:46:37 +0200

    make v2DATA.up() idempotent

--- lib/captain_hook/migrations/v2_data.ex
+++ lib/captain_hook/migrations/v2_data.ex
@@ -1,17 +1,21 @@
 defmodule CaptainHook.Migrations.V2Data do
   def up do
-    CaptainHook.list_webhook_endpoints(includes: [:enabled_notification_types])
+    CaptainHook.list_webhook_endpoints(includes: [:enabled_notification_types, :secret])
     |> Enum.each(fn webhook_endpoint ->
-      {:ok, _} =
-        CaptainHook.WebhookEndpoints.Secrets.create_webhook_endpoint_secret(
-          webhook_endpoint,
-          webhook_endpoint.started_at
-        )
+      unless webhook_endpoint.secret do
+        {:ok, _} =
+          CaptainHook.WebhookEndpoints.Secrets.create_webhook_endpoint_secret(
+            webhook_endpoint,
+            webhook_endpoint.started_at
+          )
+      end
 
-      {:ok, _} =
-        CaptainHook.update_webhook_endpoint(webhook_endpoint, %{
-          enabled_notification_types: [%{name: "*"}]
-        })
+      if length(webhook_endpoint.enabled_notification_types) == 0 do
+        {:ok, _} =
+          CaptainHook.update_webhook_endpoint(webhook_endpoint, %{
+            enabled_notification_types: [%{name: "*"}]
+          })
+      end
     end)
   end
 
